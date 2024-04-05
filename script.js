e3649a9d8fd96893027f983edce76e4615f0b5f4
NkanyisoMz <104601010+NkanyisoMz@users.noreply.github.com>
2024-04-05 13:36:19 +0200

    event listeners added
    Button event listeners has been add and linked to buttons.

--- script.js
+++ script.js
@@ -1,82 +1,41 @@
-let msg = "Hello World";
-console.log(msg);
-
 let strings = ["rock", "paper", "scissor"];
 
-function getComputerChoice(){
-    let computer_choice = Math.floor(Math.random() * strings.length);
-    return computer_choice;
-
+function getComputerChoice() {
+    let computer_choice_index = Math.floor(Math.random() * strings.length);
+    return strings[computer_choice_index];
 }
 
-let computer_choice = " ";
+function playRound(playerSelection, computerSelection) {
+    let tester = true;
+    while(tester){
 
-let user_choice = " ";
-
-
-
-function playRound(playerSelection, computerSelection){
-
-    if(playerSelection === computerSelection){
-        return "Its a tie";
+        tester = false;
     }
-    else if (playerSelection === "rock" && computerSelection=== "paper") {
-        return "you lose! paper beats rock";
-    } 
-    
-    else if(playerSelection === "paper" && computerSelection === "scissor"){
-        return "you lose! scissor beats paper";
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie");
+    } else if ((playerSelection === "rock" && computerSelection === "paper") ||
+               (playerSelection === "paper" && computerSelection === "scissor") ||
+               (playerSelection === "scissor" && computerSelection === "rock")) {
+        console.log("You lose! " + computerSelection + " beats " + playerSelection);
+    } else {
+        console.log("You win!");
     }
-    else if(playerSelection === "scissor" && computerSelection === "rock"){
-        return "you lose! rock beats scissor";
-    }
-    else {
-        return "You win!";
-        
-    }
-    
-
 }
 
-function playGame(){
-    let count = 0;
-    let userCount = 0;
-    let computerCount = 0;
-    while(count < 5){
-        user_choice = prompt("Please enter  rock, paper or  scissor").toLowerCase();
-        computer_choice = strings[getComputerChoice()];
-        console.log(playRound(user_choice, computer_choice));
-        let result = playRound(user_choice, computer_choice);
-        if(result=== "You win!"){
-            userCount++;
-        }else if(result.startsWith("you lose")){
-            computerCount++;
-        }
-
-
-        count++;
-    }
-
-    if(userCount> computerCount){
-        console.log("You won the game");
-    }
-    else if(computerCount > userCount){
-        console.log("The computer won the game");
-    }else{
-        console.log("its a tie");
-    }
-
+function handleButtonClick(event) {
+    let playerChoice = event.target.id;
+    let computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
 }
 
-playGame();
-  
+document.addEventListener("DOMContentLoaded", function() {
+    // Your JavaScript code here
+    const button1 = document.getElementById("rock");
+    const button2 = document.getElementById("paper");
+    const button3 = document.getElementById("scissor");
 
-
-
-
-
-
- 
-
-
-
+    // Add event listeners to the buttons for the "click" event
+    button1.addEventListener("click", handleButtonClick);
+    button2.addEventListener("click", handleButtonClick);
+    button3.addEventListener("click", handleButtonClick);
+});
